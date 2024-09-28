edb36f202697e1dee4dea4813e5de69bc44a788b
Serhii Shuliak <sergshulya@gmail.com>
2024-09-28 17:12:47 +0300

    fix db cleanup

--- models/message.rb
+++ models/message.rb
@@ -4,11 +4,19 @@ require 'sqlite3'
 
 class Message
   def self.cleanup(db)
-    db.execute('DELETE FROM messages WHERE created_at < ?', [Time.now.beginning_of_day.to_i])
+    db.execute('DELETE FROM messages WHERE created_at < ?', [Time.now.to_i])
   end
 
   def self.messages_today(db)
-    db.execute('SELECT * FROM messages WHERE created_at > ?', [Time.now.beginning_of_day.to_i])
+    db.execute('SELECT * FROM messages WHERE created_at > ?', [start_of_today])
+  end
+
+  def self.start_of_today
+    now = Time.now
+
+    start_of_day = Time.new(now.year, now.month, now.day)
+
+    start_of_day.to_i
   end
 
   def initialize(user, message, date, db:)
